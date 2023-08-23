46efe93a9252868dcd1effc27f70b045f4b7d5de
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-08-23 08:54:55 +0530

    do it

---
+++ cpp programming/pattern2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){ 
+    int i,j,k,n;
+    cout<<"enter the value of n ";
+    cin>>n; 
+    for(i=1;i<=n;i++){
+        for(j=n;j>i;j--){
+            cout<<" ";
+        }
+        for(k=1;k<=i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
