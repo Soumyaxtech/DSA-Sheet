608dae312566a5d3f99580e61a1a7df5f5433cc8
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-08-27 09:28:18 +0530

    do it

--- cpp programming/firstlastdigit.cpp
+++ cpp programming/firstlastdigit.cpp
@@ -2,20 +2,31 @@
 
 using namespace std;
 int main(){
-    int num,rem;
+    int num,rem,temp;
     cout<<"enter the number ";
     cin>>num;
-if(num>0){
+if(num>=0){
     rem=num%10;
     cout<<"the last digit of the number is "<<rem<<endl;
 }
-    while(num>10){
+        while(num>10){
         num=num/10;
     }
         cout<<"the first digit of the number is "<<num<<endl;
         cout<<"the sum of first and last digit of a number is "<<rem+num<<endl;
         cout<<"the multiple of first and last digit of a number is "<<rem*num<<endl;
-
-
+        //swapping useing 3rd variable
+        temp=num;
+        num=rem;
+        rem=temp;
+        cout<<"after swapping the value of first digit is "<<num<<endl;
+        cout<<"after swapping the value of last digit is "<<rem<<endl;
+        //swapping withoutn 3rd variable
+        num=num+rem;
+        rem=num-rem;
+        num=num-rem;
+        cout<<"after swapping the value of first digit is "<<num<<endl;
+        cout<<"after swapping the value of last digit is "<<rem<<endl;
+        
     return 0;
 }
--- cpp programming/tempCodeRunnerFile.cpp
+++ cpp programming/tempCodeRunnerFile.cpp
@@ -1,4 +1,5 @@
- while(num>10){
-        num=num/10;
-    }
-        cout<<"the first digit of the number is "<<num<<endl;
+ // temp=num;
+        // num=rem;
+        // rem=temp;
+        // cout<<"after swapping the value of first digit is "<<num<<endl;
+        // cout<<"after swapping the value of last digit is "<<rem<<endl;
