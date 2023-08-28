c05ec89440bf1430dc9df1c57b5bd08c48f8720d
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-08-28 15:39:11 +0530

    do it

--- cpp programming/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
- // temp=num;
-        // num=rem;
-        // rem=temp;
-        // cout<<"after swapping the value of first digit is "<<num<<endl;
-        // cout<<"after swapping the value of last digit is "<<rem<<endl;
