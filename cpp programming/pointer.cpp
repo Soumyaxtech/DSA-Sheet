d0e88305de9e11e8114c8aa5eee6d4f0a0caa117
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-03 12:32:48 +0530

    do it

--- cpp programming/pointer.cpp
+++ cpp programming/pointer.cpp
@@ -8,5 +8,9 @@ int main(){
      cout<<"the adress of a is "<<&a<<endl;
      cout<<"the value that store in pointer variable b is "<<*b<<endl;/* "*"is called dereference operator
                                              it shows value of operator*/
+    int** c=&b;//pointer to pointer(store adress of a pointer)   
+      cout<<"the adress of pointer b is "<<c<<endl;  
+      cout<<"the adress of pointer b is "<<&b<<endl;  
+      cout<<"the value at adress value_at(value at_c) "<<**c<<endl;                                
     return 0;
 }
