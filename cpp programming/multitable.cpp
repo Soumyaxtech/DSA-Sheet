09f3aea8470efbc0897542a420582ada368c230e
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-03 09:23:17 +0530

    do it

--- cpp programming/multitable.cpp
+++ cpp programming/multitable.cpp
@@ -2,12 +2,18 @@
 
 using namespace std;
 int main(){
-    int i,num,mul;
+    int i=1,num,mul;
     cout<<"enter the number ";
     cin>>num;
-    for(i=1;i<=10;i++){
+    // for(i=1;i<=10;i++){
+    //     mul=num*i;
+    //     cout<<num<< "*" <<i<<" = "<<mul<<endl;
+    // }
+    do{
         mul=num*i;
-        cout<<num<< "*" <<i<<" = "<<mul<<endl;
+        cout<<num<<"*"<<i<<"="<<mul<<endl;
+         i++;
     }
+    while( i<=10);
     return 0;
 }
