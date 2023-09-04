d436088c2126bcecb85d8fa6b3ada942c58d879c
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-04 19:28:06 +0530

    do it

---
+++ cpp programming/array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int mark[4]={12,45,32,56};
+    cout<<mark[0]<<endl;
+    cout<<mark[1]<<endl;
+    cout<<mark[2]<<endl;
+    cout<<mark[3]<<endl;
+    return 0;
+}
