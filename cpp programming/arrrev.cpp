91ccef8cff29e213f7a14b55388915a2dc709b1c
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-06 21:45:18 +0530

    do it

--- cpp programming/arrrev.cpp
+++ cpp programming/arrrev.cpp
@@ -10,13 +10,16 @@ int main(){
         cout<<"enter the elements ";
         cin>>arr[i];
     }
-    for(i=0;i<(n/2);i++){
-        temp=arr[i];
-        arr[i]=arr[n-i-1];
-        arr[n-i-1]=temp;
-    }
-     cout<<"the reverse array is ";
-    for(i=0;i<n;i++){
+    // for(i=0;i<(n/2);i++){
+    //     temp=arr[i];
+    //     arr[i]=arr[n-i-1];
+    //     arr[n-i-1]=temp;
+    // }
+    //  cout<<"the reverse array is ";
+    // for(i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    for(i=n-1;i>=0;i--){
         cout<<arr[i]<<" ";
     }
     
