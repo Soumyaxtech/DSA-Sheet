eb894c1d9ca89f709d5567ccc555bfde1ca4d831
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-12 19:46:31 +0530

    do it

---
+++ cpp programming/unionenum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+// // typedef struct employ
+// // {
+// //     int id;
+// //     float salary;
+// //     char favchar;
+// // }ep;
+
+// union money
+// {
+//     int rice;  //4
+//     float car;  //1              //only one among this three we can use at a time
+//     char pounds;  //4   // better memory management in union than struct
+// };
+
+// int main(){
+//     union money m1;
+//     m1.rice=34;
+//     m1.car=23.7;
+//     cout<<m1.car;
+// }
+int main(){
+    enum meal{breakfast,lunch,dinner};
+    cout<<breakfast<<endl;
+    cout<<lunch<<endl;
+    cout<<dinner;
+}
