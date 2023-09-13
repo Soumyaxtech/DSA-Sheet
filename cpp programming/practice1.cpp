0ec531852a1e929d307a0e2d6205ad6adff916a2
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-13 20:05:13 +0530

    do it

--- cpp programming/practice1.cpp
+++ cpp programming/practice1.cpp
@@ -23,21 +23,37 @@
 //     }
 //     return 0;
 // }
+// #include<iostream>
+
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter the size of array ";
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cout<<"enter the elements ";
+//         cin>>arr[i];
+//     }
+
+//     for(int i=n-1;i>=0;i--){
+//         cout<<"the value of  reversen array is "<<arr[i]<<endl; 
+//     }
+//     return 0;
+// }
 #include<iostream>
 
 using namespace std;
 int main(){
-    int n;
-    cout<<"enter the size of array ";
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cout<<"enter the elements ";
-        cin>>arr[i];
+    int rem,num,a=0,b=1;
+    cout<<"enter the number ";
+    cin>>num;
+    while(num>0){
+        rem=num%2;
+        num=num/2;
+        a=a+(b*rem);
+        b=b*10;
     }
-
-    for(int i=n-1;i>=0;i--){
-        cout<<"the value of  reversen array is "<<arr[i]<<endl; 
-    }
+    cout<<"the binary form of the decimal number is "<<a;
     return 0;
 }
