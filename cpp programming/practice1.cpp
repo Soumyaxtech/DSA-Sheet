6fc99ef22a654ae3d6f5b9bda29391622152e603
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-09-13 19:53:53 +0530

    do it

--- cpp programming/practice1.cpp
+++ cpp programming/practice1.cpp
@@ -1,25 +1,43 @@
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+// int main(){
+//     int n,d,rem,sum=0;
+//     cout<<"enter num of digit in number ";
+//     cin>>d;
+//     cout<<"enter the number ";
+//     cin>>n;
+//     int x=n;
+
+//     while(x>0){
+//         rem=x%10;
+//         sum=sum+pow(rem,d);
+//        x=x/10;
+
+//     }
+//     if(sum==n){
+//         cout<<"the number is a amstrong number ";
+//     }
+//     else{
+//         cout<<"the number is not a amstrong number ";
+//     }
+//     return 0;
+// }
 #include<iostream>
-#include<cmath>
+
 using namespace std;
 int main(){
-    int n,d,rem,sum=0;
-    cout<<"enter num of digit in number ";
-    cin>>d;
-    cout<<"enter the number ";
+    int n;
+    cout<<"enter the size of array ";
     cin>>n;
-    int x=n;
-
-    while(x>0){
-        rem=x%10;
-        sum=sum+pow(rem,d);
-       x=x/10;
-
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter the elements ";
+        cin>>arr[i];
     }
-    if(sum==n){
-        cout<<"the number is a amstrong number ";
-    }
-    else{
-        cout<<"the number is not a amstrong number ";
+
+    for(int i=n-1;i>=0;i--){
+        cout<<"the value of  reversen array is "<<arr[i]<<endl; 
     }
     return 0;
 }
