19dfbee560f1e0a74ed8a5cd1089965e22f49ab6
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-11-02 09:49:59 +0530

    do it

--- cpp programming/struct.cpp
+++ cpp programming/struct.cpp
@@ -1,20 +1,37 @@
+// #include<iostream>
+
+// using namespace std;
+// typedef struct employ
+// {
+//     int id;
+//     float salary;
+//     char favchar;
+// }ep;
+// int main(){
+//     // struct employ
+//      ep babai;
+//     babai.id=12356;
+//     babai.salary=56900.67;
+//     babai.favchar='A';
+//     cout<<"the id of employ babai is "<<babai.id<<endl;
+//     cout<<"the salary of employ babai is "<<babai.salary<<endl;
+//     cout<<"the favchar of employ babai is "<<babai.favchar<<endl;
+//     return 0;
+// }
 #include<iostream>
 
 using namespace std;
-typedef struct employ
+struct rectangle
 {
-    int id;
-    float salary;
-    char favchar;
-}ep;
+    int length,breadth;
+    rectangle(int l,int b)
+    {
+        length=l;
+        breadth=b;
+    }
+};
 int main(){
-    // struct employ
-     ep babai;
-    babai.id=12356;
-    babai.salary=56900.67;
-    babai.favchar='A';
-    cout<<"the id of employ babai is "<<babai.id<<endl;
-    cout<<"the salary of employ babai is "<<babai.salary<<endl;
-    cout<<"the favchar of employ babai is "<<babai.favchar<<endl;
-    return 0;
+    struct rectangle r(2,2);
+        cout<<r.length*r.breadth<<endl;
+
 }
