d279d04ae930083f9a4fe1ba72c6ecf3c7f740e4
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-11-02 07:39:07 +0530

    do it

--- cpp programming/practice1.cpp
+++ cpp programming/practice1.cpp
@@ -1,135 +1,135 @@
+// // #include<iostream>
+// // #include<cmath>
+// // using namespace std;
+// // int main(){
+// //     int n,d,rem,sum=0;
+// //     cout<<"enter num of digit in number ";
+// //     cin>>d;
+// //     cout<<"enter the number ";
+// //     cin>>n;
+// //     int x=n;
+
+// //     while(x>0){
+// //         rem=x%10;
+// //         sum=sum+pow(rem,d);
+// //        x=x/10;
+
+// //     }
+// //     if(sum==n){
+// //         cout<<"the number is a amstrong number ";
+// //     }
+// //     else{
+// //         cout<<"the number is not a amstrong number ";
+// //     }
+// //     return 0;
+// // }
+// // #include<iostream>
+
+// // using namespace std;
+// // int main(){
+// //     int n;
+// //     cout<<"enter the size of array ";
+// //     cin>>n;
+// //     int arr[n];
+// //     for(int i=0;i<n;i++){
+// //         cout<<"enter the elements ";
+// //         cin>>arr[i];
+// //     }
+
+// //     for(int i=n-1;i>=0;i--){
+// //         cout<<"the value of  reversen array is "<<arr[i]<<endl; 
+// //     }
+// //     return 0;
+// // }
+// // #include<iostream>
+
+// // using namespace std;
+// // int main(){
+// //     int rem,num,a=0,b=1;
+// //     cout<<"enter the number ";
+// //     cin>>num;
+// //     while(num>0){
+// //         rem=num%2;
+// //         num=num/2;
+// //         a=a+(b*rem);
+// //         b=b*10;
+// //     }
+// //     cout<<"the binary form of the decimal number is "<<a;
+// //     return 0;
+// // }
+// // #include<iostream>
+
+// // using namespace std;
+// // int main(){
+// //     int i,n,j;
+// //     cout<<"enter the num of rows ";
+// //     cin>>n;
+// //     for(i=0;i<n;i++){
+// //         for(j=0;j<i;j++){
+// //             cout<<" ";
+// //         }
+// //         for(int k=n;k>i;k--){
+// //             cout<<"*";
+// //         }
+// //         cout<<endl;
+// //     }
+// //     return 0;
+// // }
+// // #include<iostream>
+
+// // using namespace std;
+// // int main(){
+// //     int a=0,b=1,c,n;
+// //     cout<<"enter the range ";
+// //     cin>>n;
+// //     // cout<<a<<endl;
+// //     // cout<<b<<endl;
+// //     for(int i=2;i<n;i++){
+// //         c=a+b;
+// //         a=b;
+// //         b=c;
+// //     }
+// //      cout<<c<<endl;
+// //     return 0;
+// // }
+// // #include<iostream>
+// // #include<cmath>
+// // using namespace std;
+// // int main(){
+// //     int n,rem,sum=0;
+// //     cout<<"enter the number ";
+// //     cin>>n;
+// //     int x=n;
+// //     while(x>0){
+// //         rem=x%10;
+// //         x=x/10;
+// //         sum=sum+rem;
+// //     }
+// //     cout<<"the sum of the digits are "<<sum;
+// //     return 0;
+// // }
 // #include<iostream>
-// #include<cmath>
+
 // using namespace std;
 // int main(){
-//     int n,d,rem,sum=0;
-//     cout<<"enter num of digit in number ";
-//     cin>>d;
-//     cout<<"enter the number ";
+//     int i,j,k,n;
+//     cout<<"enter the number of rows ";
 //     cin>>n;
-//     int x=n;
-
-//     while(x>0){
-//         rem=x%10;
-//         sum=sum+pow(rem,d);
-//        x=x/10;
-
-//     }
-//     if(sum==n){
-//         cout<<"the number is a amstrong number ";
-//     }
-//     else{
-//         cout<<"the number is not a amstrong number ";
-//     }
-//     return 0;
-// }
-// #include<iostream>
-
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter the size of array ";
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cout<<"enter the elements ";
-//         cin>>arr[i];
-//     }
-
-//     for(int i=n-1;i>=0;i--){
-//         cout<<"the value of  reversen array is "<<arr[i]<<endl; 
-//     }
-//     return 0;
-// }
-// #include<iostream>
-
-// using namespace std;
-// int main(){
-//     int rem,num,a=0,b=1;
-//     cout<<"enter the number ";
-//     cin>>num;
-//     while(num>0){
-//         rem=num%2;
-//         num=num/2;
-//         a=a+(b*rem);
-//         b=b*10;
-//     }
-//     cout<<"the binary form of the decimal number is "<<a;
-//     return 0;
-// }
-// #include<iostream>
-
-// using namespace std;
-// int main(){
-//     int i,n,j;
-//     cout<<"enter the num of rows ";
-//     cin>>n;
+//     // char l=65;
 //     for(i=0;i<n;i++){
-//         for(j=0;j<i;j++){
+//         // char l=65;
+//         // for(k=0;k<=i;k++){
+//         //     cout<<l;
+//         //     l++;
+//         for(j=n-1;j>i;j--){
 //             cout<<" ";
 //         }
-//         for(int k=n;k>i;k--){
-//             cout<<"*";
+//          char l=65;
+//         for(k=0;k<=i;k++){
+//             cout<<l;
+//             l++;
 //         }
 //         cout<<endl;
 //     }
 //     return 0;
 // }
-// #include<iostream>
-
-// using namespace std;
-// int main(){
-//     int a=0,b=1,c,n;
-//     cout<<"enter the range ";
-//     cin>>n;
-//     // cout<<a<<endl;
-//     // cout<<b<<endl;
-//     for(int i=2;i<n;i++){
-//         c=a+b;
-//         a=b;
-//         b=c;
-//     }
-//      cout<<c<<endl;
-//     return 0;
-// }
-// #include<iostream>
-// #include<cmath>
-// using namespace std;
-// int main(){
-//     int n,rem,sum=0;
-//     cout<<"enter the number ";
-//     cin>>n;
-//     int x=n;
-//     while(x>0){
-//         rem=x%10;
-//         x=x/10;
-//         sum=sum+rem;
-//     }
-//     cout<<"the sum of the digits are "<<sum;
-//     return 0;
-// }
-#include<iostream>
-
-using namespace std;
-int main(){
-    int i,j,k,n;
-    cout<<"enter the number of rows ";
-    cin>>n;
-    // char l=65;
-    for(i=0;i<n;i++){
-        // char l=65;
-        // for(k=0;k<=i;k++){
-        //     cout<<l;
-        //     l++;
-        for(j=n-1;j>i;j--){
-            cout<<" ";
-        }
-         char l=65;
-        for(k=0;k<=i;k++){
-            cout<<l;
-            l++;
-        }
-        cout<<endl;
-    }
-    return 0;
-}
