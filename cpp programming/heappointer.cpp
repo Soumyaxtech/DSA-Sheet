4a7c88cf3229432e581cbee5184d5c107c63590b
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2023-11-04 10:03:48 +0530

    do it

--- cpp programming/heappointer.cpp
+++ cpp programming/heappointer.cpp
@@ -1,36 +1,36 @@
-#include<iostream>
-//accussing array element from heap memory by pointer
-using namespace std;
-int main(){
-    int *p;
-    p=new int[4];//here array elkements styored in heap memory
-    p[0]=13;p[1]=14;p[2]=45;p[3]=15;
-    for(int i=0;i<4;i++){
-        cout<<p[i]<<endl;
-    }
+// #include<iostream>
+// //accussing array element from heap memory by pointer
+// using namespace std;
+// int main(){
+//     int *p;
+//     p=new int[4];//here array elkements styored in heap memory
+//     p[0]=13;p[1]=14;p[2]=45;p[3]=15;
+//     for(int i=0;i<4;i++){
+//         cout<<p[i]<<endl;
+//     }
   
   
 
 
 //pointer size is independent of its datatypes
-// #include<iostream>
+#include<iostream>
 
-// using namespace std;
-// struct rectangle{
-//     int length;
-//     int breadth;
-// };
-// int main(){
-//     int *p;
-//     float*p1;
-//     char *p2;
-//     double *p3;
-//     struct rectangle *p4;
-//     cout << sizeof(p)<<endl;
-//      cout << sizeof(p1)<<endl;
-//       cout << sizeof(p2)<<endl;
-//        cout << sizeof(p3)<<endl;
-//         cout << sizeof(p4)<<endl;
+using namespace std;
+struct rectangle{
+    int length;
+    int breadth;
+};
+int main(){
+    int *p;
+    float*p1;
+    char *p2;
+    double *p3;
+    struct rectangle *p4;
+    cout << sizeof(p)<<endl;
+     cout << sizeof(p1)<<endl;
+      cout << sizeof(p2)<<endl;
+       cout << sizeof(p3)<<endl;
+        cout << sizeof(p4)<<endl;
 
-// return 0;
-// }
+return 0;
+}
